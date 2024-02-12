5b5571a7bb02be5fab6143245aff64bc9088040c
Stalloyde <stalloyde93@gmail.com>
2024-02-12 15:31:18 +0800

    Update project & CV

--- src/projects.js
+++ src/projects.js
@@ -1,22 +1,23 @@
 import techAndThreads from './project-screenshots/Tech&Threads.png';
 import battleshipImg from './project-screenshots/Battleship-screenshot.png';
-import membersOnlyImg from './project-screenshots/members-only-screenshot.png';
+import blogApiImg from './project-screenshots/blogapi-screenshot.png';
 
 const projects = [
   {
-    name: 'Members Only',
+    name: 'Blog Site',
     description:
-      'A forum where anyone can post and view anonymous messages. VIP users can view the username of messages. MODERATORS, who are invited to the role with a special password, have additional details of date & time posted and the feature to delete messages. ',
+      'A full-stack single author blog site with public access for reading. Authentication is required to leave comments.',
     description2:
-      'INVITATION PASSWORD TO MODERATOR ACCOUNT IS NOT AVAILABLE TO PUBLIC',
-    techStack: ['NodeJS', 'Express','MongoDB', 'Pug', 'PassportJS', 'CSS'],
-    repo: 'https://github.com/Stalloyde/members-only',
-    live: 'https://members-only-stalloyde.fly.dev/',
-    imageUrl: membersOnlyImg,
+      'The project includes RESTFUL API and a private CMS site where the author can perform CRUD operations on posts and comments.',
+    techStack: ['NodeJS', 'Express', 'MongoDB', 'React', 'Typescript', 'CSS'],
+    repo: 'https://github.com/Stalloyde/blogapi-client',
+    live: 'https://blogapi-client-stalloyde.vercel.app/posts',
+    imageUrl: blogApiImg,
   },
   {
     name: 'Tech & Threads',
-    description: 'A front-end e-commerce site with products fetched from FakeStoreAPI, shopping cart feature and full mobile responsiveness.',
+    description:
+      'A front-end e-commerce site with products fetched from FakeStoreAPI, shopping cart feature and full mobile responsiveness.',
     techStack: ['HTML', 'CSS', 'TypeScript', 'React', 'Vitest'],
     repo: 'https://github.com/Stalloyde/shopping-cart',
     live: 'https://shopping-cart-green-one.vercel.app/',
@@ -31,7 +32,6 @@ const projects = [
     live: 'https://stalloyde.github.io/battleship/',
     imageUrl: battleshipImg,
   },
-
 ];
 
 export default projects;
